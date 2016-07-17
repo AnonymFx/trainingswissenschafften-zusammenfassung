16d055c3244b3427c6821ee1af870f47266231c5
Nils Kunze <kunze.nils@gmail.com>
2016-07-17 14:05:28 +0200

    page 19 technik2

--- test.py
+++ test.py
@@ -3,33 +3,21 @@
 from re import sub
 
 inp = """
-•
-•
-•
-•
-Kontraktion im Rumpf vor Bewegung
-(Feed-Forward)
-Schutzfunktion für die Wirbelsäule
-Biomechanische Vorrausetzung für
-Wurf-, Schlag-, Schussbewegungen
-Belastungsnormative
-–
-–
-–
-–
-niedrigen Intensitäten (25%)
-recht lange (30s)
-statisch und dynamisch
-keine Periodisierung, trainingsbegleitend
+• Freiheitsgrade:
+Sukzessives Freisetzen, „selective defrosting“
+• Gestalt:
+flüssige, lockere Bewegung, Kombinationen
+• Methodik:
+Intensive Rückmeldungen, große Wiederholungszahlen
 """
 
 inp = inp.replace('\n', '')
 print(inp)
 
-inp = sub(r'•([\w ]*)–', r'\n\\item \1:', inp)
+# inp = sub(r'•([\w ]*)–', r'\n\\item \1:', inp)
 
 
-# inp = inp.replace('•', '\n\item ')
+inp = inp.replace('•', '\n\item ')
 inp = inp.replace('–', ',')
 
 print(inp)
